d342c8d0b34bc92233f3f3b8941ec028f0e4ffe1
pazrein <pazreingold1996@gmail.com>
2023-03-04 21:05:04 +0200

    Flip Ifs

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -47,11 +47,10 @@ class GildedRose {
                 if (item.name.equals("Backstage passes to a Metallica concert")) {
                     item.quality = 0;
                 } else {
-                    if (item.quality > 0) {
-                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                    if (!item.name.equals("Sulfuras, Hand of Ragnaros"))
+                        if (item.quality > 0) {
                             item.quality = item.quality - 1;
                         }
-                    }
                 }
             }
         }
