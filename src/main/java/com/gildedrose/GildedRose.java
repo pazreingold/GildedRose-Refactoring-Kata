afcec68d6cce52a90754144a469caca94e49d10d
pazrein <pazreingold1996@gmail.com>
2023-03-04 21:48:23 +0200

    Move all Methods to ItemCategory

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -11,61 +11,8 @@ class GildedRose {
 
     public void updateItems() {
         ItemCategory category = new ItemCategory();
-        Arrays.stream(items).forEach(item -> update(item, category));
-    }
-
-    private void update(Item item, ItemCategory category) {
-        updateQualityOf(item, category);
-        updateSellInOf(item, category);
-        if (item.sellIn < 0) {
-            updateExpired(item, category);
-        }
-    }
-
-    private void updateQualityOf(Item item, ItemCategory category) {
-        if (item.name.equals("Aged Brie")) {
-            increaseQualityOf(item, category);
-
-        } else if (item.name.equals("Backstage passes to a Metallica concert")) {
-            updateByDays(item, 11, category);
-            updateByDays(item, 6, category);
-            increaseQualityOf(item, category);
-
-        } else if (!item.name.equals("Sulfuras, Hand of Ragnaros"))
-            decreaseQualityOf(item, category);
-    }
-
-    private void updateSellInOf(Item item, ItemCategory category) {
-        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            item.sellIn = item.sellIn - 1;
-        }
-    }
-
-    private void updateExpired(Item item, ItemCategory category) {
-        if (item.name.equals("Aged Brie")) {
-            increaseQualityOf(item, category);
-        } else if (item.name.equals("Backstage passes to a Metallica concert")) {
-            item.quality = 0;
-        } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-        } else decreaseQualityOf(item, category);
+        Arrays.stream(items).forEach(item -> category.update(item));
     }
 
 
-    private void increaseQualityOf(Item item, ItemCategory category) {
-        if (item.quality < 50) {
-            item.quality = item.quality + 1;
-        }
-    }
-
-    private void decreaseQualityOf(Item item, ItemCategory category) {
-        if (item.quality > 0) {
-            item.quality = item.quality - 1;
-        }
-    }
-
-    private void updateByDays(Item item, int days, ItemCategory category) {
-        if (item.sellIn < days) {
-            increaseQualityOf(item, category);
-        }
-    }
 }
--- src/main/java/com/gildedrose/ItemCategory.java
+++ src/main/java/com/gildedrose/ItemCategory.java
@@ -1,4 +1,57 @@
 package com.gildedrose;
 
 public class ItemCategory {
+    void update(Item item) {
+        updateQualityOf(item);
+        updateSellInOf(item);
+        if (item.sellIn < 0) {
+            updateExpired(item);
+        }
+    }
+
+    void updateQualityOf(Item item) {
+        if (item.name.equals("Aged Brie")) {
+            increaseQualityOf(item);
+
+        } else if (item.name.equals("Backstage passes to a Metallica concert")) {
+            updateByDays(item, 11);
+            updateByDays(item, 6);
+            increaseQualityOf(item);
+
+        } else if (!item.name.equals("Sulfuras, Hand of Ragnaros"))
+            decreaseQualityOf(item);
+    }
+
+    void updateSellInOf(Item item) {
+        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            item.sellIn = item.sellIn - 1;
+        }
+    }
+
+    void updateExpired(Item item) {
+        if (item.name.equals("Aged Brie")) {
+            increaseQualityOf(item);
+        } else if (item.name.equals("Backstage passes to a Metallica concert")) {
+            item.quality = 0;
+        } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+        } else decreaseQualityOf(item);
+    }
+
+    void updateByDays(Item item, int days) {
+        if (item.sellIn < days) {
+            increaseQualityOf(item);
+        }
+    }
+
+    void increaseQualityOf(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
+        }
+    }
+
+    void decreaseQualityOf(Item item) {
+        if (item.quality > 0) {
+            item.quality = item.quality - 1;
+        }
+    }
 }
