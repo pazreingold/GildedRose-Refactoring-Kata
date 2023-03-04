14524091856691b10b79c29e422a1fd4fa85dffc
pazrein <pazreingold1996@gmail.com>
2023-03-04 20:47:23 +0200

    The Condition Apply to All Statements

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -22,24 +22,24 @@ class GildedRose {
     private void updateQualityOf(Item item) {
         if (item.name.equals("Aged Brie")
                 || item.name.equals("Backstage passes to a Metallica concert")) {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
+            }
+            if (item.name.equals("Backstage passes to a Metallica concert")) {
+                if (item.sellIn < 11) {
                     if (item.quality < 50) {
                         item.quality = item.quality + 1;
-
-                        if (item.name.equals("Backstage passes to a Metallica concert")) {
-                            if (item.sellIn < 11) {
-                                if (item.quality < 50) {
-                                    item.quality = item.quality + 1;
-                                }
-                            }
-
-                            if (item.sellIn < 6) {
-                                if (item.quality < 50) {
-                                    item.quality = item.quality + 1;
-                                }
-                            }
-                        }
                     }
-                } else {
+                }
+
+                if (item.sellIn < 6) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                }
+            }
+
+        } else {
             if (item.quality > 0) {
                 if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
                     item.quality = item.quality - 1;
